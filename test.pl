bc384502afe874133abe22120944f375aac8be54
tonycrotwell <tony@crotwell.net>
2017-05-23 14:28:25 -0400

    Create test.pl

---
+++ test.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+$| = 1;
+sub slowcat {
+foreach $byte (split //, $string) {
+	print $byte;
+	select (undef,undef,undef, 0.000000001); 
+}
+}
+$clear="/usr/bin/clear";
+system($clear);
+open(MYFILE, "./153") || die "Can't open zones_q: $!";
+while(<MYFILE>) {
+$string=$_;
+&slowcat($string);
+	if (/\*\*\*\*\*\*\*\*\*\*\*\*/) {
+	print "Your Answer: ";
+	chomp($text = <STDIN>); 
+	$text=~tr/a-z/A-Z/;
+        print $_; 
+}
+}
